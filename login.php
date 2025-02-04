c01d110c88f15d8e2fc7a70dbb33c80d58825ad5
leobaksaj <baksajleo22@gmail.com>
2025-02-04 13:23:32 +0100

    Dodan login.php za vjezbu

---
+++ login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="assets/style.css">
+    </head>
+    <body>
+
+        <h1>Lista automobila</h1>
+        <?php require "includes/obrazac.php"; ?>
+
+    </body>
+</html>
